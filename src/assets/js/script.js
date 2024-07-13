b1d0f5c32b88f7fc0377ab6d90841e5db2817d5f
Andrii Krasovskyi <andriikr@compatibl.com>
2024-07-13 16:52:11 +0200

    Add code

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,3 +4,39 @@ const header = document.querySelector("header");
 burgerMenuToggler.addEventListener("click", (e) => {
   header.classList.toggle("opened");
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const copyButton = document.querySelector('.btn-copy');
+  const referalText = document.querySelector('.referal').textContent;
+
+  copyButton.addEventListener('click', function() {
+    navigator.clipboard.writeText(referalText)
+        .then(() => {
+          const originalText = copyButton.textContent;
+          copyButton.textContent = 'Copied ✔';
+          setTimeout(() => {
+            copyButton.textContent = originalText;
+          }, 1000);
+        })
+        .catch(err => {
+          console.error('Failed to copy text: ', err);
+        });
+  });
+
+    const progressBarLine = document.querySelector('.progress-bar__line');
+    const startValue = parseFloat(document.querySelector('.progress-start-val').textContent);
+    const finishValue = parseFloat(document.querySelector('.progress-finish-val').textContent);
+
+    let percentage = (startValue / finishValue) * 100;
+    if (percentage < 2) {
+      percentage = 2;
+    }
+
+    if (percentage > 100) {
+      percentage = 100;
+    }
+
+    progressBarLine.style.width = `${percentage}%`;
+
+});
